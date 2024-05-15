9edcc953d9d107a85f42e96d2f4925409b72ee91
Tim Mitra <tim@it-guy.com>
2024-05-15 13:39:41 -0400

    renamed lessons and add lesson02 app for review

---
+++ 02-sort-filter-relationships/Final/GoodDogDemo/GoodDog/Parks/NewParkView.swift
@@ -0,0 +1,44 @@
+//
+//  NewParkView.swift
+//  MyJobs
+//
+//  Created by Tim Mitra on 2023-12-03.
+//
+
+import SwiftUI
+import SwiftData
+
+struct NewParkView: View {
+  @Environment(\.modelContext) var modelContext
+  @Environment(\.dismiss) var dismiss
+  @State private var name = ""
+  
+    var body: some View {
+      NavigationStack {
+        GroupBox {
+          LabeledContent {
+            TextField("Name", text: $name)
+          } label: {
+            Text("Name")
+              .foregroundStyle(.secondary)
+          }
+          Button("Add Park") {
+            let newPark = ParkModel(name: name)
+            modelContext.insert(newPark)
+            try! modelContext.save()
+            dismiss()
+          }
+          .buttonStyle(.borderedProminent)
+          .disabled(name.isEmpty)
+          Spacer()
+        }
+        .padding()
+        .navigationTitle("New Park") // this works on macOS
+      }
+    }
+}
+
+#Preview {
+    NewParkView()
+    .modelContainer(for: ParkModel.self)
+}
