4c37430248518c459a0a92662723b17dda4c1177
Tim Mitra <tim@it-guy.com>
2024-06-06 12:04:01 -0400

    add SwiftData to ParkView

--- 02-sort-filter-relationships/Final/GoodDog/GoodDog/Parks/ParksView.swift
+++ 02-sort-filter-relationships/Final/GoodDog/GoodDog/Parks/ParksView.swift
@@ -6,22 +6,27 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ParksView: View {
   @Environment(\.dismiss) private var dismiss
   @State private var newPark = false
+  @Environment(\.modelContext) private var modelContext
+  @Query(sort: \ParkModel.name) var parks: [ParkModel]
+  @Bindable var dog: DogModel
   
   var body: some View {
     NavigationStack {
       Group {
+        if !parks.isEmpty {
           List {
-            ForEach(1...4, id:\.self) { park in
-              Text("Nice Park")
+            ForEach(parks) { park in
+              Text(park.name)
             }
           }
           LabeledContent {
             Button {
-              
+              // addRemove will go here
             } label: {
               Image(systemName: "plus.circle.fill")
                 .imageScale(.large)
@@ -32,17 +37,34 @@ struct ParksView: View {
               .font(.caption)
               .foregroundStyle(.secondary)
           }
+        } else {
+          ContentUnavailableView {
+            Image(systemName: "tree")
+          } description: {
+            Text("You need to create some parks.")
+          } actions: {
+            Button("Create Park") {
+              newPark.toggle()
+            }
+            .buttonStyle(.borderedProminent)
+          }
         }
+      }
       .navigationTitle("My Dog's parks")
       .sheet(isPresented: $newPark) {
         NewParkView()
       }
     }
   }
-
-
-
+}
 
 #Preview {
-  ParksView()
+  let container = try! ModelContainer(for: DogModel.self)
+  let parks = [
+    ParkModel(name: "Riverdale"),
+    ParkModel(name: "Withrow")
+  ]
+  let dog = DogModel(name: "Mac", parks: parks)
+  return ParksView(dog: dog)
+    .modelContainer(container)
 }
