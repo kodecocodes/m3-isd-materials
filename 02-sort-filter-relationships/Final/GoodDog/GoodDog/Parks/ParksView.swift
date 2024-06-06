36628084337a233eeb5a03d87b60f39478c7541e
Tim Mitra <tim@it-guy.com>
2024-06-06 12:42:43 -0400

    update Foreach

--- 02-sort-filter-relationships/Final/GoodDog/GoodDog/Parks/ParksView.swift
+++ 02-sort-filter-relationships/Final/GoodDog/GoodDog/Parks/ParksView.swift
@@ -21,7 +21,29 @@ struct ParksView: View {
         if !parks.isEmpty {
           List {
             ForEach(parks) { park in
-              Text(park.name)
+              HStack {
+                if let dogParks = dog.parks {
+                  if dogParks.isEmpty {
+                    Button {
+                      addRemove(park)
+                    } label: {
+                      Image(systemName: "circle")
+                    }
+                  } else {
+                    Button {
+                      addRemove(park)
+                    } label: {
+                      Image(
+                        systemName: 
+                          dogParks.contains(
+                            park
+                        ) ? "circle.fill" : "circle"
+                      )
+                    }
+                  }
+                }
+                Text(park.name)
+              }
             }
           }
           LabeledContent {
@@ -53,6 +75,26 @@ struct ParksView: View {
       .navigationTitle("My Dog's parks")
       .sheet(isPresented: $newPark) {
         NewParkView()
+      }
+    }
+  }
+  func addRemove(_ park: ParkModel) {
+    if let dogParks = dog.parks {
+      // check if parks is empty
+      if dogParks.isEmpty {
+        dog.parks?.append(park)
+      } else {
+        // check if park is associated
+        // remove park if true
+        // add park if false
+        if dogParks.contains(park),
+            let index = dogParks.firstIndex(where: {
+              $0.id == park.id
+            }) {
+          dog.parks?.remove(at: index)
+        } else {
+          dog.parks?.append(park)
+        }
       }
     }
   }
