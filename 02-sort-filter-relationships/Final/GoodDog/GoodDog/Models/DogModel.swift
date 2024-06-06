31eb97f5a396c928b1057db6fd6a44d1328efa54
Tim Mitra <tim@it-guy.com>
2024-06-06 03:46:48 -0400

    update mock data

--- 02-sort-filter-relationships/Final/GoodDog/GoodDog/Models/DogModel.swift
+++ 02-sort-filter-relationships/Final/GoodDog/GoodDog/Models/DogModel.swift
@@ -32,6 +32,7 @@
 
 import Foundation
 import SwiftData
+import UIKit
 
 @Model
 class DogModel {
@@ -74,48 +75,19 @@ extension DogModel {
     let bouvier = BreedModel(name: "Bouvier")
     let mixed = BreedModel(name: "Mixed")
     
-    // dogs
-    let macDog = DogModel(
-      name: "Mac",
-      age: 11,
-      weight: 90,
-      color: "Yellow",
-      breed: labrador,
-      image: nil
-    )
-    let sorcha = DogModel(
-      name: "Sorcha",
-      age: 1,
-      weight: 40,
-      color: "Yellow",
-      breed: golden,
-      image: nil
-    )
-    let violet = DogModel(
-      name: "Violet",
-      age: 4,
-      weight: 85,
-      color: "Gray",
-      breed: bouvier,
-      image: nil
-    )
-    let kirby = DogModel(
-      name: "Kirby",
-      age: 11,
-      weight: 95,
-      color: "Fox Red",
-      breed: labrador,
-      image: nil
-    )
-    let priscilla = DogModel(
-      name: "Priscilla",
-      age: 17,
-      weight: 65,
-      color: "White",
-      breed: mixed,
-      image: nil
-    )
-    
+    let riverdale = ParkModel(name: "Riverdale Park")
+    let withrow = ParkModel(name: "Withrow Park")
+    let greenwood = ParkModel(name: "Greewood Park")
+    let hideaway = ParkModel(name: "Hideaway Park")
+    let kewBeach = ParkModel(name: "Kew Beach Off Leash Dog Park")
+    let allan = ParkModel(name: "Allan Gardens")
+
+    let macDog = DogModel(name: "Mac", age: 11, weight: 90, color: "Yellow", breed: labrador, image: UIImage(resource: .macintosh).pngData()!, parks: [riverdale, withrow, kewBeach])
+    let sorcha = DogModel(name: "Sorcha", age: 1, weight: 40, color: "Yellow", breed: golden, image: UIImage(resource: .sorcha).pngData()!, parks: [greenwood, withrow])
+    let violet = DogModel(name: "Violet", age: 4, weight: 85, color: "Gray", breed: bouvier, image: UIImage(resource: .violet).pngData()!, parks: [riverdale, withrow, hideaway])
+    let kirby = DogModel(name: "Kirby", age: 11, weight: 95, color: "Fox Red", breed: labrador, image: UIImage(resource: .kirby).pngData()!, parks: [allan, greenwood, kewBeach])
+    let priscilla = DogModel(name: "Priscilla", age: 17, weight: 65, color: "White", breed: mixed, image: nil, parks: [])
+
     
     container.mainContext.insert(macDog)
     container.mainContext.insert(sorcha)
